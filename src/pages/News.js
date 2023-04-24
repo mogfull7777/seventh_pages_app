bd8ec0b956db6c3a3fd9c61907c75cc23df8da24
mogfull7777 <anjunyoung94@gmail.com>
2023-04-24 22:26:44 +0900

    fixed

--- src/pages/News.js
+++ src/pages/News.js
@@ -20,9 +20,9 @@ const News = () => {
             console.log(err);
         }
 
-    if (setNews) {
-        return null;
-    }
+    // if (setNews) {
+    //     return null;
+    // }
 
     };
 
@@ -43,9 +43,12 @@ const News = () => {
                         <th>더보기</th>
                     </tr>
                     </thead>
-                    {news && news.data.null == news.data.title && news.map(n => (
+                    {news && news.map(n => (
                         <tbody>
                         <tr>
+                            {/*<td>{n.title === null*/}
+                            {/*    ? "타이틀 없음"*/}
+                            {/*    : n.title.slice(0, 10)}</td>*/}
                             <td>{n.title.slice(0, 10)}</td>
                             <td>{n.author}</td>
                             <td>{n.content.slice(0, 30)}</td>
@@ -62,5 +65,8 @@ const News = () => {
 export default News;
 
 // 1. null에 할당값을 넣으려고 했으나 실패.
-// 2. null을 뺸다??
-// 3. 스크립트 로드 우선순위가 잘못되었거나, 로드가 되지 않았거나, 스크립트가 로드될 당시 페이지에 아이디나 클래스에 해당하는 요소가 없거나 한 경우
+// 2. api에 articles.title을 분석??
+// 3. 함수로서 slice() 에 대해 서치
+// 4. 스크립트 로드 우선순위가 잘못되었거나, 로드가 되지 않았거나, 스크립트가 로드될 당시 페이지에 아이디나 클래스에 해당하는 요소가 없거나 한 경우.
+
+//
